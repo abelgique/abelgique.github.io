4c9cd3178b7df42a6a3861b431a46fc32467555f
abelgique <alejandrobelgique@gmail.com>
2025-02-09 12:54:10 -0800

    idk why its a different color but ok i hope it works (added coins and stuff)

--- programming/sillygame/script.js
+++ programming/sillygame/script.js
@@ -5,9 +5,23 @@ const Player = {
     y        : 0,
     speed    : 5,
     updatePos: function() {
-        this.element.style = "top: "+ this.y +"px; left:"+ this.x +"px;";
+        this.element.style = "top: "+ this.y +"px; left: "+ this.x +"px;";
     }
 }
+
+// coin object
+const Coin = {
+    element: document.getElementById("coin"),
+    x      : 0,
+    y      : 0,
+    newPos : function() {
+        this.x = Math.floor(Math.random() * 461);
+        this.y = Math.floor(Math.random() * 461);
+        this.element.style = "top: "+ this.y +"px; left: "+ this.x +"px;";
+    }
+}
+// first coin position
+Coin.newPos();
 
 // movement
 window.addEventListener("keydown", function(input) {
@@ -24,9 +38,11 @@ window.addEventListener("keydown", function(input) {
             Player.y -= Player.speed;
         }
     } else if ((input.code == "KeyS") || (input.code == "ArrowDown")) {
-        if (Player.y < 470) {
+        if (Player.y < 460) {
             Player.y += Player.speed;
         }
+    } else if (input.code == "KeyR") {
+        Coin.newPos();
     }
     Player.updatePos();
 });
