b504bbd9985d576705ff4b0b6a5b964a1ac11d79
abelgique <alejandrobelgique@gmail.com>
2025-02-09 11:56:01 -0800

    test

---
+++ programming/sillygame/script.js
@@ -0,0 +1,21 @@
+const Player = {
+    element  : document.getElementById("player"),
+    x        : 0,
+    y        : 0,
+    speed    : 5,
+    updatePos: function() {
+        this.element.style = "position: absolute; top: "+ this.y +"; left:"+ this.x +";";
+    },
+    moveRight: function() {
+        this.x += this.speed;
+        this.updatePos();
+    }
+}
+
+window.addEventListener("keydown", decode);
+
+function decode(input) {
+    if (input.code == "KeyD" || input.code == "ArrowRight") {
+        Player.moveRight()
+    }
+}
